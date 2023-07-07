332fd590abc0dd6924de465cc3f39732fcf7f15c
qhuy135520 <letranquochuy1305@gmail.com>
2023-07-07 23:49:01 +0700

    second-tests

--- src/components/DateContainer.jsx
+++ src/components/DateContainer.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DateDisplay from "./DateDisplay";
 import "./date.css"
 const DateContainer = () => {
     const [date, setDate] = useState(Date.now())
 
+    useEffect(() => {
+        console.log(rerendered)
+    }, [date])
     const handleDateChange = (e) => {
         console.log(e);
 
